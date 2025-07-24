d619dd9ec5594c4693954e051aad7a1d94723e12
yuzhengchua <yuzheng94@gmail.com>
2025-07-24 17:41:29 +0800

    Fix: Prevent CinemaAppRunner from running during tests by setting test profile in @SpringBootTest. Remove main method test to avoid blocking input.

--- src/test/java/com/yuzhengchua/cinema/CinemaApplicationTest.java
+++ src/test/java/com/yuzhengchua/cinema/CinemaApplicationTest.java
@@ -3,14 +3,10 @@ package com.yuzhengchua.cinema;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class CinemaApplicationTest {
     @Test
     void contextLoads() {
         // This will pass if the Spring context loads successfully
-    }
-    @Test
-    void testMain() {
-        CinemaApplication.main(new String[]{});
     }
 } 
